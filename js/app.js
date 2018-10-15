d6966b4e5697baa8c18f0e966410145cabd4937b
Daniel Mnich <daniel.mnich@spook.se>
2018-10-15 22:57:09 +0200

    work in progress

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", main());
-function main (){
+
+this.startGame = function(){
+	//clearing the board as the game can start multiple times one after another
+	this.gameBoard = document.querySelector('.game-board');
+	this.gameBoard.innerHTML = '';
+	
+	//clearing the game score
+	this.gameScore = document.querySelector(".game-score");
+	this.gameScore.innerHTML = "";
+	
+	//clearing the variables
+	let 
 	
 }
